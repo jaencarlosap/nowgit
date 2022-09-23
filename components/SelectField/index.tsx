234d376501e90e9083976382b640320391f1e517
janper231 <jeancarloss659@gmail.com>
2022-09-22 23:42:19 -0500

    feat: save repos in localStorage

--- components/SelectField/index.tsx
+++ components/SelectField/index.tsx
@@ -1,31 +1,28 @@
-export interface SelectFieldProps extends React.AllHTMLAttributes<HTMLSelectElement> {
-	label: string;
-	options: { value: string; label: string; }[]
-}
+import Select, { Props } from 'react-select'
 
-export const SelectField = ({
-	label,
-	options,
-	...selectProps
-}: SelectFieldProps) => {
+export const SelectField = (selectProps: Props) => {
+	const customStyles = {
+		control: (provided: any) => ({
+			...provided,
+			border: 'none',
+			boxShadow: 'none',
+			'&:hover': {
+				borderColor: 'red'
+			}
+		}),
+		singleValue: (provided: any, state: any) => {
+			const opacity = state.isDisabled ? 0.5 : 1;
+			// const transition = 'opacity 300ms';
+
+			return { ...provided, opacity };
+		}
+	}
+
 	return (
-		<>
-			<label
-				htmlFor={selectProps?.name || selectProps?.id}
-				className="sr-only"
-			>
-				{label}
-			</label>
-			<select
-				className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-				{...selectProps}
-				defaultValue='@'
-			>
-				<option value='@'>{label}</option>
-				{options && options.map((item, index) =>
-					<option key={index} value={item.value}>{item.label}</option>
-				)}
-			</select>
-		</>
+		<Select
+			styles={customStyles}
+			className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+			{...selectProps}
+		/>
 	)
 }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,26 +1,21 @@
 import React, { useEffect, useState } from "react"
 import Router from 'next/router'
-import {
-	ItemRepositorie,
-	SelectField,
-	SelectFieldProps
-} from "components"
+import { ItemRepositorie, SelectField } from "components"
 import { InfoPrProps, MainPrProps } from "interfaces/infoPr"
 import { getInfoPr } from "services"
 
 const Dashboard = () => {
-	const [selectData, setSelectData] = useState<SelectFieldProps['options']>([])
+	const [selectData, setSelectData] = useState<{ value: string; label: string; }[]>([])
 	const [listRepos, setListRepos] = useState<InfoPrProps[]>([])
+	const [selectedOption, setSelectedOption] = useState<unknown>(null)
 
-	const handleSaveParams = async (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault()
-		const target = event.target as HTMLFormElement
-		const repositorieValue = target.repositorie.value.split('/')
+	const handleSaveParams = async (event: React.MouseEvent) => {
+		const repositorieValue = (selectedOption as { value: string; }).value.split('/')
 		const dataToQuery = { ownerRepo: repositorieValue[0], nameRepo: repositorieValue[1] }
 		const { repository } = await getDataToList(dataToQuery)
-		target.repositorie.value = ''
 		const prevLocal = JSON.parse(localStorage.getItem('repositories') || '[]')
 		localStorage.setItem('repositories', JSON.stringify([...prevLocal, dataToQuery]))
+		setSelectedOption(null)
 		setListRepos((beforeList) => ([
 			...beforeList,
 			{
@@ -40,10 +35,17 @@ const Dashboard = () => {
 	useEffect(() => {
 		const handleGetData = async () => {
 			const repositories = await (await fetch('/api/github/getRepositories')).json()
-			const selectData = repositories?.viewer?.repositories?.nodes?.map(({ nameWithOwner }: { nameWithOwner: string }) => {
+			const Data = repositories?.viewer?.repositories?.nodes?.map(({ nameWithOwner }: { nameWithOwner: string }) => {
 				return { value: nameWithOwner, label: nameWithOwner }
 			})
-			setSelectData(selectData)
+			let newList = []
+			const repositoriesSaved = JSON.parse(localStorage.getItem('repositories') || '[]')
+			for (const selectedOption of repositoriesSaved) {
+				const { repository } = await getDataToList(selectedOption)
+				newList.push({ ...selectedOption, repository })
+			}
+			setListRepos(newList)
+			setSelectData(Data)
 		}
 
 		handleGetData()
@@ -51,12 +53,17 @@ const Dashboard = () => {
 
 	return (
 		<div className="flex w-full flex-1 flex-col px-10">
-			<form className="flex space-x-5" onSubmit={handleSaveParams}>
-				<SelectField label="Select one repositorie" name="repositorie" options={selectData} />
-				<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+			<div className="flex space-x-5 w-1/3">
+				<SelectField
+					isSearchable
+					placeholder="Select one repositorie"
+					onChange={setSelectedOption}
+					options={selectData}
+				/>
+				<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800" onClick={handleSaveParams}>
 					Add
 				</button>
-			</form>
+			</div>
 			<section className="flex flex-wrap">
 				{listRepos.length === 0 && (
 					<div className="flex-row w-full p-6 text-center">
